129d7e899b6c4c14b6950b87c76f90a2443ed8f2
Nikolay Kashev <nikolay.kashev@gmail.com>
2026-01-15 13:08:56 +0200

    Update nav.js

--- en/assets/js/nav.js
+++ en/assets/js/nav.js
@@ -9,7 +9,7 @@
             <div class="container">
                 <div class="nav-wrapper">
                     <a href="/en/" class="logo">
-                        <img src="../assets/images/bimi-svg-tiny-12-ps.svg" width="40px" height="auto" alt="LifeHack Software Logo">
+                        <img src="../assets/images/logo.svg" width="40px" height="auto" alt="LifeHack Software Logo">
                         <span>LifeHack Software</span>
                     </a>
                     <button class="mobile-menu-toggle" aria-label="Toggle menu">
@@ -116,5 +116,6 @@
         initNav();
     }
 })();
+
 
 
